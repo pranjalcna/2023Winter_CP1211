37c0c067b9c11ae484be052e94cf8ce43651d043
pranjalcna <pranjal.cna@gmail.com>
2023-02-06 14:33:03 -0330

    extra files

--- 3-JS/3.2-js-examples/script.js
+++ 3-JS/3.2-js-examples/script.js
@@ -1,1 +1,5 @@
-alert("Hi")
+function convert() {
+    let tempInCelcius = parseFloat(document.querySelector("#cel-temp").value)
+    tempInFahr = (tempInCelcius * 1.8) + 32
+    document.querySelector('#result').innerHTML = `${tempInCelcius}℃ = ${tempInFahr}℉`
+}
