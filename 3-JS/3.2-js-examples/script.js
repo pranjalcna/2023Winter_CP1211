4869940de9b43156358016e7608a2d781524e336
pranjalcna <pranjal.cna@gmail.com>
2023-02-07 10:05:10 -0330

    add function to script.js

--- 3-JS/3.2-js-examples/script.js
+++ 3-JS/3.2-js-examples/script.js
@@ -1,5 +1,5 @@
 function convert() {
-    let tempInCelcius = parseFloat(document.querySelector("#cel-temp").value)
-    tempInFahr = (tempInCelcius * 1.8) + 32
-    document.querySelector('#result').innerHTML = `${tempInCelcius}℃ = ${tempInFahr}℉`
+    let tempInCelcius = parseFloat(document.querySelector("#cel-temp").value);
+    tempInFahr = (tempInCelcius * 1.8) + 32;
+    document.querySelector('#result').innerHTML = `${tempInCelcius}℃ = ${tempInFahr}℉`;
 }
