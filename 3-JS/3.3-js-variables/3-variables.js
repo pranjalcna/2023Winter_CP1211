0ae2e4a445f0471a59f451b8f4f76dd32c8205a8
pranjalcna <pranjal.cna@gmail.com>
2023-02-13 11:53:58 -0330

    example demoing prompts and alerts

---
+++ 3-JS/3.3-js-variables/3-variables.js
@@ -0,0 +1,6 @@
+const miles = parseFloat(prompt("Number of miles: "))
+const gallons = parseFloat(prompt("Number of gallons: "))
+
+const milesPerGallon = miles/gallons
+
+alert(`${milesPerGallon} miles per gallon`)
